ff7e90582dae16a67998a87128bb77ccde07cf39
Mintaee <mintae020990@naver.com>
2025-04-16 18:53:35 +0900

    Fix: update twoSum logic

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print "Hello,Git"
+from typing import List
+def twoSum(nums: List[int], target: int) -> List[int]:
+    arr = {}
+    for i, num in enumerate(nums):
+        cp = target - num
+        if cp in arr:
+            return [arr[cp], i]
+        arr[num] = i
